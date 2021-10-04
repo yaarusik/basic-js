27a97372bd9022947637a2b859012b382e2b064b
Ruslan <yaarusik@gmail.com>
2021-10-04 21:12:49 +0500

    feat: finish

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,20 +16,17 @@ import { NotImplementedError } from "../extensions/index.js";
  *
  */
 export default function repeater(str, options) {
-   let strAdd = [];
-   let strR = [];
-   for(let i = 0; i < (options.additionRepeatTimes || 1); i++){
-   strAdd.push(options.addition);}
-   let resStrAdd = strAdd.join(options.additionSeparator || ‘|’);
-   for(let i = 0; i < (options.repeatTimes || 1); i++){
-   strR.push(str + resStrAdd);}
-   return strR.join(options.separator || ‘+’);
+  let strAdd = [];
+  let strR = [];
+  if (options.addition === null) {
+    options.addition = "null";
+  }
+  for (let i = 0; i < (options.additionRepeatTimes || 1); i++) {
+    strAdd.push(options.addition);
+  }
+  let resStrAdd = strAdd.join(options.additionSeparator || `|`);
+  for (let i = 0; i < (options.repeatTimes || 1); i++) {
+    strR.push(str + resStrAdd);
+  }
+  return strR.join(options.separator || `+`);
 }
-// console.log(
-//   repeater("la", {
-//     repeatTimes: 3,
-//     separator: "s",
-//     addition: "+",
-//     additionRepeatTimes: 1,
-//   })
-// );
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,87 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(x = true) {
+    this.x = x;
+    this.start = 65;
+    this.end = 90;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    let arr = [];
+    let count = 0;
+
+    if (message == null || key == null) {
+      throw new Error("Incorrect arguments!");
+    }
+    let keyUp = key.toUpperCase();
+    let newMessage = message.toUpperCase();
+    for (let i = 0; i < newMessage.length; i++) {
+      if (
+        newMessage.charCodeAt(i) >= this.start &&
+        newMessage.charCodeAt(i) <= this.end
+      ) {
+        arr.push(
+          String.fromCharCode(
+            ((newMessage.charCodeAt(i) +
+              keyUp.charCodeAt(count % key.length) -
+              130) %
+              26) +
+              this.start
+          )
+        );
+        count++;
+      } else {
+        arr.push(newMessage[i]);
+      }
+    }
+    return this.x ? arr.join("") : arr.reverse().join("");
+  }
+  decrypt(message, key) {
+    let arr = [];
+    let count = 0;
+    if (message == null || key == null) {
+      throw new Error("Incorrect arguments!");
+    }
+    let keyUp = key.toUpperCase();
+    let newMessage = message.toUpperCase();
+    for (let i = 0; i < newMessage.length; i++) {
+      if (
+        newMessage.charCodeAt(i) >= this.start &&
+        newMessage.charCodeAt(i) <= this.end
+      ) {
+        arr.push(
+          String.fromCharCode(
+            ((newMessage.charCodeAt(i) +
+              26 -
+              keyUp.charCodeAt(count % key.length)) %
+              26) +
+              this.start
+          )
+        );
+        count++;
+      } else {
+        arr.push(newMessage[i]);
+      }
+    }
+    return this.x ? arr.join("") : arr.reverse().join("");
   }
 }
